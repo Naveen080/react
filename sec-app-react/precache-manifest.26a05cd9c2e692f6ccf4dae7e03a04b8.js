82533c104179dbca3866aec6eb66ee9f95885c5e
naveen-19 <naveen.jains@sasken.com>
2020-05-15 12:10:36 +0530

    adding react

---
+++ sec-app-react/precache-manifest.26a05cd9c2e692f6ccf4dae7e03a04b8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9abe03af8d8586967946ac159b84aa3d",
+    "url": "/sec-app-react/index.html"
+  },
+  {
+    "revision": "07a432372b1259ec8c2d",
+    "url": "/sec-app-react/static/css/2.d3b1b87c.chunk.css"
+  },
+  {
+    "revision": "d5c9a4dcdefe94917a6c",
+    "url": "/sec-app-react/static/css/main.5379e78c.chunk.css"
+  },
+  {
+    "revision": "07a432372b1259ec8c2d",
+    "url": "/sec-app-react/static/js/2.9374fc9d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/sec-app-react/static/js/2.9374fc9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5c9a4dcdefe94917a6c",
+    "url": "/sec-app-react/static/js/main.393f66ac.chunk.js"
+  },
+  {
+    "revision": "73a6bc80c1f691ab05c4",
+    "url": "/sec-app-react/static/js/runtime-main.7a0a00ce.js"
+  },
+  {
+    "revision": "57ee984832a55e2ca6d47e5f17f67ef3",
+    "url": "/sec-app-react/static/media/SEC_Logo.57ee9848.png"
+  },
+  {
+    "revision": "ca52fb4d140b7d8f72ae89718c3c8a68",
+    "url": "/sec-app-react/static/media/fotter_img.ca52fb4d.png"
+  }
+]);
